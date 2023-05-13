a4e9db44ccf04117d39900ed80c534d4e32dc4bd
Ahmed Moamen <76903727+ahmedmoamen1@users.noreply.github.com>
2023-05-13 14:24:44 +0300

    Update Multiplier.v

--- Verilog Code Building Blocks/Multiplier.v
+++ Verilog Code Building Blocks/Multiplier.v
@@ -22,36 +22,29 @@
 
 module Multiplier(input clock, start, [7:0] multiplier, [7:0] multiplicand, output  sign,  output reg zflag, output reg [13:0] result);
 wire [6:0] comp_multiplier;
-wire [6:0] outputMultiplier;
+wire [7:0] outputMultiplier;
 wire [6:0] outputShiftMultiplier;
 reg xnoroutput;
 wire [6:0] comp_multiplicand;
-wire [6:0] outputMultiplicand;
-wire [6:0] outputShiftMultiplicand;
+wire [7:0] outputMultiplicand;
+wire [13:0] outputShiftMultiplicand;
 reg [13:0] output14bitadder;
 wire [13:0] outputmux3;
 wire  [13:0] outputdff;
-wire [13:0]bit14 = {7'b0000000, outputMultiplicand};
 
 assign sign = (multiplicand[7]  ^ multiplier[7]);
 
-twos_complement t1(.input_number(multiplier[6:0]),.twos_comp(comp_multiplier));
+mux #(8)m1(.x(multiplier[7:0]),.y({~multiplier[7:0] + 1'b1}),.sel(multiplier[7]),.out(outputMultiplier));
 
-mux m1(.x(multiplier[6:0]),.y(comp_multiplier),.sel(multiplier[7]),.out(outputMultiplier));
+shift_right_register #(7)sr1(clock, start, ~start, outputMultiplier[6:0], outputShiftMultiplier);
 
-shift_right_register #(7)sr1(clock, start, ~start, outputMultiplier, outputShiftMultiplier);
+mux #(8)m2(.x(multiplicand[7:0]), .y({~multiplicand[7:0] + 1'b1}), .sel(multiplicand[7]), .out(outputMultiplicand));
 
-twos_complement t2(.input_number(multiplicand[6:0]), .twos_comp(comp_multiplicand));
-
-mux m2(.x(multiplicand[6:0]), .y(comp_multiplicand), .sel(multiplicand[7]), .out(outputMultiplicand));
-
-shift_left_register sr2(clock, start, ~start, bit14, outputShiftMultiplicand);
-
-//bit14carryadder adder(1'b0, multiplier, multiplier, output14bitadder);
-
+shift_left_register#(14) sr2(clock, start, ~start, {7'b0000000, outputMultiplicand[6:0]}, outputShiftMultiplicand);
 
 mux #(14)m3(outputdff, output14bitadder, outputShiftMultiplier[0], outputmux3);
 DataReg d1(clock, start,~zflag, outputmux3, outputdff);
+
 
 always @(*) begin
     xnoroutput <= ~(outputShiftMultiplier[0] | outputShiftMultiplier[1] | outputShiftMultiplier[2] | outputShiftMultiplier[3] | outputShiftMultiplier[4] | outputShiftMultiplier[5] | outputShiftMultiplier[6]);  
